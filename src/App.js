ad3642ed9cc3c674523697986ffcb0b465d03b68
Daenam Kim <daenam.kim@gmail.com>
2019-03-30 16:04:49 +0900

    Add the numnber of players and contract balance

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      manager: '',
-    };
-  }
+  state = {
+    manager: '',
+    players: [],
+    balance: '0',
+  };
 
   async componentDidMount() {
     console.log(web3.version);
@@ -18,15 +16,21 @@ class App extends Component {
 
     // default { from: accounts[0] } of call's parameter
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getAllPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
   }
 
   render() {
-    const { manager } = this.state;
+    const { manager, players, balance } = this.state;
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {manager}</p>
+        <p>This contract is managed by {manager}.</p>
+        <p>
+          There are currently {players.length} people entered, competing to win
+          {web3.utils.fromWei(balance, 'ether')} ether!
+        </p>
       </div>
     );
   }
